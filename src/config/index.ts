aae3378ec6246237cd0c770bb85cf6a12c21e3ad
me <me@edcilo.com>
2024-01-05 00:00:53 +0000

    feat(version): add landing version on footer
    - try another nextjs icon

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,5 +1,8 @@
 export default {
+    app: {
+        version: import.meta.env.APP_VERSION,
+    },
     ga4: {
-        id: import.meta.env.PUBLIC_GA4_MEASUREMENT_ID
+        id: import.meta.env.PUBLIC_GA4_MEASUREMENT_ID,
     }
 }
